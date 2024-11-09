6ab490033cd1db29be9dae9667ff83e224b0e3f0
hechmi10 <hechmi.jaidane@gmail.com>
2024-11-09 12:27:46 +0100

    first commit

---
+++ src/main/java/com/example/examen2022/Examen2022Application.java
@@ -0,0 +1,13 @@
+package com.example.examen2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examen2022/Examen2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examen2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
